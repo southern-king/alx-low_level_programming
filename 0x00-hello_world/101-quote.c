13f5c4b52385279a7444f2c1a6564557c0985bc0
southern-king <omokemillen824@gmail.com>
2023-02-16 13:24:31 -0800

    Simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description - 'This is a C program that prints a line to standard error'
+ * Return: This returns 1
+ */
+
+int main(void);
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
