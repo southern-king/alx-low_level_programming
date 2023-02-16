a618741c77e9683a9eb502feda03336a00bea897
southern-king <omokemillen824@gmail.com>
2023-02-16 12:28:12 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#!/bin/bash
-int main(void) 
-{ 
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - 'this is a program that prints using the puts function'
+ * Return: This returns 0
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
- return (0);
+return (0);
 }
