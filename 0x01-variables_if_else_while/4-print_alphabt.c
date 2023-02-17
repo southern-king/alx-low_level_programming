296fa4ec355ac9ce2dfa02dcc626b0f139c61101
southern-king <omokemillen824@gmail.com>
2023-02-17 12:15:58 -0800

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ * Return: This is 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+	return (0);
+}
