dacdadb3b051e11e97492194537450c739d1a1b4
southern-king <omokemillen824@gmail.com>
2023-02-17 11:57:04 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point, prints the alphabet
+ * Return: This is 0
+ */
+int main(void);
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
